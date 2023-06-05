71463961a0d1e751f7e961e645ff2511e2c23263
WickerbeastII <exequiel.place@hotmail.com>
2023-06-05 16:30:33 -0300

    creaci'on del backend de java

---
+++ JavaBackend/src/main/java/com/example/modelos/Usuario.java
@@ -0,0 +1,16 @@
+package com.example.modelos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+    private String user;
+    private String password;
+    private String role;
+}
